f5464c5f7faed4988421ffd71f5764b0ead2940a
zc <syfhub@hotmail.com>
2023-04-24 16:17:07 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -33,23 +33,23 @@ function font(woff2) { return `<link rel="preload" as="font" type="font/woff2" c
  * the assets to load in the macro
  */
 const theme_stylesheets = [
-    "styles/stmaterial.css", // all the css created for this specific theme
+  "styles/stmaterial.css", // all the css created for this specific theme
 ];
 const theme_scripts = [
-    "scripts/materialize.js",
-    "scripts/stmaterial.js",
+  "scripts/materialize.js",
+  "scripts/stmaterial.js",
 ];
 const ma_stylesheets = [
-    `vendor/material-icons/${maVersions.maIcons}/iconfont/material-icons.css`,
+  `vendor/material-icons/${maVersions.maIcons}/iconfont/material-icons.css`,
 ];
 const ma_fonts = [
-    `vendor/material-icons/${maVersions.maIcons}/iconfont/material-icons.woff2`,
-    `vendor/material-icons/${maVersions.maIcons}/iconfont/material-icons.woff`,
+  `vendor/material-icons/${maVersions.maIcons}/iconfont/material-icons.woff2`,
+  `vendor/material-icons/${maVersions.maIcons}/iconfont/material-icons.woff`,
 ];
 
 function macroTemplate({ compilation }) {
 
-    return dedent(`\
+  return dedent(`\
         <!--
             AUTO-GENERATED from webpack.config.js, do **NOT** edit by hand.
             These are re-used in layout.html
@@ -78,68 +78,68 @@ function macroTemplate({ compilation }) {
 }
 
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-    filename: resolve(staticPath, "webpack-macros.html"),
-    inject: false,
-    minify: false,
-    css: true,
-    templateContent: macroTemplate,
+  filename: resolve(staticPath, "webpack-macros.html"),
+  inject: false,
+  minify: false,
+  css: true,
+  templateContent: macroTemplate,
 });
 /*******************************************************************************/
 
 const copyPlugin = new CopyPlugin({
-    patterns: [
-        {
-            context: "./node_modules/material-icons",
-            from: "LICENSE",
-            to: resolve(maPath.maIcons, "LICENSE.txt"),
-        },
-        {
-            context: "./node_modules/material-icons/iconfont",
-            from: "filled.css",
-            to: resolve(maPath.maIcons, "iconfont", "material-icons.css"),
-        },
-        {
-            context: "./node_modules/material-icons/iconfont",
-            from: "material-icons.woff2",
-            to: resolve(maPath.maIcons, "iconfont"),
-        },
-        {
-            context: "./node_modules/material-icons/iconfont",
-            from: "material-icons.woff",
-            to: resolve(maPath.maIcons, "iconfont"),
-        },
-    ]
+  patterns: [
+    {
+      context: "./node_modules/material-icons",
+      from: "LICENSE",
+      to: resolve(maPath.maIcons, "LICENSE.txt"),
+    },
+    {
+      context: "./node_modules/material-icons/iconfont",
+      from: "filled.css",
+      to: resolve(maPath.maIcons, "iconfont", "material-icons.css"),
+    },
+    {
+      context: "./node_modules/material-icons/iconfont",
+      from: "material-icons.woff2",
+      to: resolve(maPath.maIcons, "iconfont"),
+    },
+    {
+      context: "./node_modules/material-icons/iconfont",
+      from: "material-icons.woff",
+      to: resolve(maPath.maIcons, "iconfont"),
+    },
+  ]
 });
 
 
 module.exports = {
-    mode: "production",
-    devtool: "source-map",
-    entry: {
-        "stmaterial": [
-            resolve(scriptPath, "stmaterial.js"),
-            resolve(stylePath, "stmaterial.sass"),
-        ],
-        "materialize": resolve(scriptPath, "materialize.js"),
-    },
-    output: { filename: "scripts/[name].js", path: staticPath },
-    plugins: [new MiniCssExtractPlugin({ filename: "styles/[name].css" }), htmlWebpackPlugin, copyPlugin],
-    optimization: { minimizer: [`...`, new CssMinimizerPlugin()] },
-    module: {
-        rules: [{
-            test: /\.s[ac]ss$/i,
-            use: [
-                MiniCssExtractPlugin.loader,
-                { loader: "css-loader", options: { url: false, } },
-                { loader: "postcss-loader", options: { sourceMap: true } },
-                {
-                    loader: "sass-loader", options: {
-                        sourceMap: true,
-                        implementation: require("sass"),
-                        sassOptions: { outputStyle: "expanded" }
-                    }
-                },
-            ],
-        }],
-    },
+  mode: "production",
+  devtool: "source-map",
+  entry: {
+    "stmaterial": [
+      resolve(scriptPath, "stmaterial.js"),
+      resolve(stylePath, "stmaterial.sass"),
+    ],
+    "materialize": resolve(scriptPath, "materialize.js"),
+  },
+  output: { filename: "scripts/[name].js", path: staticPath },
+  plugins: [new MiniCssExtractPlugin({ filename: "styles/[name].css" }), htmlWebpackPlugin, copyPlugin],
+  optimization: { minimizer: [`...`, new CssMinimizerPlugin()] },
+  module: {
+    rules: [{
+      test: /\.s[ac]ss$/i,
+      use: [
+        MiniCssExtractPlugin.loader,
+        { loader: "css-loader", options: { url: false, } },
+        { loader: "postcss-loader", options: { sourceMap: true } },
+        {
+          loader: "sass-loader", options: {
+            sourceMap: true,
+            implementation: require("sass"),
+            sassOptions: { outputStyle: "expanded" }
+          }
+        },
+      ],
+    }],
+  },
 };
