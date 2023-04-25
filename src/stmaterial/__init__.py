9a45563f8ad45f8b5d03d777e36f7174a251e3ac
zc <syfhub@hotmail.com>
2023-04-25 14:54:30 +0800

    bump dev

--- src/stmaterial/__init__.py
+++ src/stmaterial/__init__.py
@@ -17,7 +17,7 @@ from .utils import get_theme_options, activate_extensions, config_provided_by_us
 from .directives import GalleryDirective
 
 
-__version__ = "0.0.6"
+__version__ = "0.0.7.dev"
 logger = logging.getLogger(__name__)
 
 MESSAGE_CATALOG_NAME = "stmaterial"
